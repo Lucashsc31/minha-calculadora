a80ae48048747ff3cd651de03df6541dac790387
Lucas Henrique <lucashsc31@gmail.com>
2018-08-10 02:31:00 -0300

    Funcionalidade pronta

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -8,14 +8,24 @@ import { CalculadoraService } from './calculadora.service'
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent{
-  //teste: any
-  num: string[]
-  ope: string[]
+  expr: string = ""
+  result: number = 0
 
-  constructor(
-    private calculadoraService: CalculadoraService
-  ) { 
-    //this.teste = this.calculadoraService.teste("1+2")
+  nums: string[] = [ "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", "." ]
+  opes: string[] = [ "/", "*", "-", "+" ]
+
+  constructor(private calculadoraService: CalculadoraService) { }
+
+  addExpr(val){
+    this.expr = this.calculadoraService.addExpr(val, this.expr) 
   }
 
+  calcExpr(){
+    this.result = this.calculadoraService.calcExpr(this.expr)
+  }
+
+  clearExpr(){
+    this.expr = this.calculadoraService.clearExpr()
+    this.result = 0; // :D kkk
+  }
 }
--- src/app/calculadora/calculadora.service.ts
+++ src/app/calculadora/calculadora.service.ts
@@ -5,8 +5,35 @@ export class CalculadoraService {
 
   constructor() { }
 
-  teste(conta){
-    return eval(conta)
+  addExpr(val, atualExpr): string{
+    let _return
+    try{
+      _return = atualExpr.concat(val);
+    }catch(e){
+      console.log(e);
+      _return = "Erro"
+    }
+    return _return
+  }
+
+  clearExpr(): string{
+    return "";
+  }
+
+  calcExpr(expr): number{
+    let _return
+    try{
+      _return = eval(expr);
+      
+      if(_return === expr){
+        _return = "Erro"
+      }
+      
+    }catch(e){
+      console.log(e);
+      _return = "Erro"
+    }
+    return _return;
   }
 
 }
